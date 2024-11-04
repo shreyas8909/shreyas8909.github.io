aa9e40d18ac8252289daf95a95a9feba0b81c432
shreyasrh9 <shreyas.rh9@gmail.com>
2024-11-04 17:31:51 +0530

    Logo change

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,11 +5,17 @@ const Logo = ({ size = 'medium' }) => {
   return (
     <div className={`logo-container ${size}`}>
       <div className="logo-symbol">
-        <div className="camera-body">
-          <div className="camera-lens">
-            <div className="lens-inner"></div>
+        <div className="cctv-camera">
+          <div className="camera-body">
+            <div className="camera-lens">
+              <div className="lens-inner"></div>
+            </div>
           </div>
-          <div className="camera-detail"></div>
+          <div className="camera-mount">
+            <div className="mount-arm"></div>
+            <div className="mount-base"></div>
+          </div>
+          <div className="camera-light"></div>
         </div>
       </div>
       <span className="logo-text">
